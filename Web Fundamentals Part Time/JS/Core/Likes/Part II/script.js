25a850ee80f1893c7e523f0cd26dd69c8aacfbeb
narimenedk <narimene_dkhil@yahoo.com>
2024-02-19 23:06:15 +0000

    Likes

--- Web Fundamentals Part Time/JS/Core/Likes/Part II/script.js
+++ Web Fundamentals Part Time/JS/Core/Likes/Part II/script.js
@@ -1,37 +1,28 @@
+// JavaScript code in script.js
 
 // feed 1
-var count = 9; 
-var h3 = document.querySelector('h3');
-var countElement = document.querySelector("h3");
+var count = 9;
+var countElement = document.getElementById("count");
 
-console.log(countElement); 
-
-function add1 (){
+function add1() {
     count++;
-    countElement.innerText = count + "Like(s)";
+    countElement.innerText = count + " Like(s)";
 }
 
 // feed 2
 var count1 = 12;
-var h3 = document.querySelector('h3');
-var countElement = document.querySelector("h3");
+var countElement1 = document.getElementById("count1");
 
-console.log(countElement);
-
-function add1 (){
-    count++;
-    countElement.innerText = count + "Like(s)";
+function add2() {
+    count1++;
+    countElement1.innerText = count1 + " Like(s)";
 }
 
 // feed 3
 var count2 = 9;
-var h3 = document.querySelector('h3');
-var countElement = document.querySelector("h3");
+var countElement2 = document.getElementById("count2");
 
-console.log(countElement);
-
-function add1 (){
-count++;
-countElement.innerText = count + "Like(s)";
+function add3() {
+    count2++;
+    countElement2.innerText = count2 + " Like(s)";
 }
-    
