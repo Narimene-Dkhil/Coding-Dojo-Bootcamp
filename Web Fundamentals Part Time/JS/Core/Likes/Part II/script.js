a7d8567246daa939a76448bca306f3f694cfca9a
narimenedk <narimene_dkhil@yahoo.com>
2024-02-19 22:13:11 +0000

    Likes

---
+++ Web Fundamentals Part Time/JS/Core/Likes/Part II/script.js
@@ -0,0 +1,37 @@
+
+// feed 1
+var count = 9; 
+var h3 = document.querySelector('h3');
+var countElement = document.querySelector("h3");
+
+console.log(countElement); 
+
+function add1 (){
+    count++;
+    countElement.innerText = count + "Like(s)";
+}
+
+// feed 2
+var count1 = 12;
+var h3 = document.querySelector('h3');
+var countElement = document.querySelector("h3");
+
+console.log(countElement);
+
+function add1 (){
+    count++;
+    countElement.innerText = count + "Like(s)";
+}
+
+// feed 3
+var count2 = 9;
+var h3 = document.querySelector('h3');
+var countElement = document.querySelector("h3");
+
+console.log(countElement);
+
+function add1 (){
+count++;
+countElement.innerText = count + "Like(s)";
+}
+    
