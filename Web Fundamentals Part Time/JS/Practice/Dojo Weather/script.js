0ed39836574934f4c719afff4797c9239a657f65
narimenedk <narimene_dkhil@yahoo.com>
2024-02-27 23:28:01 +0000

    Practice

--- Web Fundamentals Part Time/JS/Practice/Dojo Weather/script.js
+++ Web Fundamentals Part Time/JS/Practice/Dojo Weather/script.js
@@ -1,44 +1,32 @@
+console.log("page loading...");
 
-console.log("Page loading...")
+var cookieDiv = document.querySelector(".cookie-policy");
 
-var cookieBoxes = document.querySelector('.cookie-policy')
-
-function loading(){
-    alert("Loading weather report")
+function loading() {
+    alert("Loading weather report...")
 }
 
-function accept(){
-    cookieBoxes.remove();
+function accept() {
+    cookieDiv.remove();
 }
 
-
-
-// We need to convert °C in °F
-function degreeInFahrenheit(temp){
-    return Math.round((9 / 5) * temp + 32);
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
 }
 
-// We need to convert °F in °C
-function fahrenheitInDegreeCelsius(temp){
-    return Math.round(((temp - 32) * 5) / 9);
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
 }
-
-
-
-// Our convert function 
 
 function convert(element) {
-    console.log(element.value)
-
-    for(var i=1; i<9; i++){
-        var tempSpan = document.querySelector('#temp' +i);
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
         var tempVal = parseInt(tempSpan.innerText);
-        if(element.value == "°C"){
-            tempSpan.innerText = fahrenheitInDegreeCelsius(tempVal)
-        }
-
-        else {
-            tempSpan.innertText = degreeInFahrenheit(tempVal)
+        if(element.value == "°C") {
+        tempSpan.innerText = f2c(tempVal);
+        } else {
+        tempSpan.innerText = c2f(tempVal);
         }
     }
 }
