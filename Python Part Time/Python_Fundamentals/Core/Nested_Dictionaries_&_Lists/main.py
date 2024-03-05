57b88317a8692b50f6fee643d25a29534f7daad1
narimenedk <narimene_dkhil@yahoo.com>
2024-03-05 21:23:30 +0000

    Nested Dictionaries and lists

--- Python Part Time/Python_Fundamentals/Core/Nested_Dictionaries_&_Lists/main.py
+++ Python Part Time/Python_Fundamentals/Core/Nested_Dictionaries_&_Lists/main.py
@@ -22,48 +22,19 @@ sports_directory['soccer'][0] = 'Andres'
 # Change the value 20 in z to 30
 z[0]['y'] = 30
 
-# Print the updated values
-print(x)
-print(students)
-print(sports_directory)
-print(z)
-
-
 # 2 - Iterate Through a List of Dictionaries
 def iterateDictionary(some_list):
     for dictionary in some_list:
-        output = ""
         for key, value in dictionary.items():
-            output += f"{key} - {value}, "
-        print(output[:-2])  # Remove the last comma and space
+            print(f"{key} - {value}")
 
-# Example usage:
-students = [
-    {'first_name': 'Michael', 'last_name': 'Jordan'},
-    {'first_name': 'John', 'last_name': 'Rosales'},
-    {'first_name': 'Mark', 'last_name': 'Guillen'},
-    {'first_name': 'KB', 'last_name': 'Tonel'}
-]
-
-iterateDictionary(students)
 
 
 # 3 - Get Values From a List of Dictionaries
 def iterateDictionary2(key_name, some_list):
     for dictionary in some_list:
-        if key_name in dictionary:
-            print(dictionary[key_name])
+        print(dictionary[key_name])
 
-# Example usage:
-students = [
-    {'first_name': 'Michael', 'last_name': 'Jordan'},
-    {'first_name': 'John', 'last_name': 'Rosales'},
-    {'first_name': 'Mark', 'last_name': 'Guillen'},
-    {'first_name': 'KB', 'last_name': 'Tonel'}
-]
-
-# Output the values of 'first_name' key for each dictionary
-iterateDictionary2('first_name', students)
 
 
 # 4 - Iterate Through a Dictionary with List Values
@@ -72,15 +43,9 @@ def printInfo(some_dict):
         print(f"{len(values)} {key.upper()}")
         for value in values:
             print(value)
+            
 
-# Example usage:
-dojo = {
-    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
 
-# Output information about the 'dojo' dictionary
-printInfo(dojo)
 
 
 
