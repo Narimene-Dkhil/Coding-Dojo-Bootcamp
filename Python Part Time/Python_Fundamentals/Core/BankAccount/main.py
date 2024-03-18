2fa44edbaff42337e4909d8d8ad6b97b93f452c7
narimenedk <narimene_dkhil@yahoo.com>
2024-03-18 12:48:36 +0000

    BankAccount

--- Python Part Time/Python_Fundamentals/Core/BankAccount/main.py
+++ Python Part Time/Python_Fundamentals/Core/BankAccount/main.py
@@ -1,6 +1,6 @@
 class BankAccount:
     
-    def __init__(self, int_rate=0.2, balance=0):
+    def __init__(self, int_rate=0.1, balance=0):
         self.int_rate = int_rate
         self.balance = balance
         
@@ -17,7 +17,7 @@ class BankAccount:
         return self 
     
     def display_account_info(self):
-        print(f"Your current balance is: {self.balance}")
+        print(f"Your current balance is: ${self.balance}")
         return self
         
     def yield_interest(self):
@@ -26,8 +26,8 @@ class BankAccount:
         return self 
     
 
-account1 = BankAccount(0.2, 100)
+account1 = BankAccount(0.1, 100)
 account1.deposit(50).deposit(30).deposit(20).withdraw(30).display_account_info().yield_interest().display_account_info()
 
-account2 = BankAccount()  # Default interest rate 0.2, default balance 0
+account2 = BankAccount(0.1, 200) 
 account2.deposit(100).deposit(50).withdraw(30).withdraw(20).withdraw(10).withdraw(5).display_account_info().yield_interest().display_account_info() 
