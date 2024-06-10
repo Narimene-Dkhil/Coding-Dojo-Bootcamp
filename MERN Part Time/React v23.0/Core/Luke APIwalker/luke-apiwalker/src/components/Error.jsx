93f3b820e08b5bc1cc9be7aec9865ccc83eb7a65
narimenedk <narimene_dkhil@yahoo.com>
2024-06-10 03:00:34 +0100

    Luke API Walker

--- MERN Part Time/React v23.0/Core/Luke APIwalker/luke-apiwalker/src/components/Error.jsx
+++ MERN Part Time/React v23.0/Core/Luke APIwalker/luke-apiwalker/src/components/Error.jsx
@@ -4,7 +4,9 @@ const Error = () => {
     return (
         <div>
             <h3>These aren't the droids you're looking for.</h3>
-            <img src="https://i.imgflip.com/5xlt6l.jpg" alt="obi wan kenobi" />
+            <img src="https://www.mensjournal.com/.image/t_share/MTk2MTM2MDQ4MDU4MjQ2NjYx/ewan-mcgregor-obi-wan-kenobi-21.jpg" 
+            alt="obi wan kenobi"
+            style={{ width: '500px', height: 'auto' }} />
         </div>
     )
 }
