60f13935fb3b4445570257e18498984ac425d628
narimenedk <narimene_dkhil@yahoo.com>
2024-07-10 22:42:28 +0100

    Game Developer 1

---
+++ C# .Net Part Time/Language Fundamentals/Core/Game Developer I/GameDev/Attack.cs
@@ -0,0 +1,15 @@
+class Attack{
+
+    public string Name;
+    public int DamageAmount;
+
+    public Attack(string name, int damage){
+        Name = name;
+        DamageAmount = damage;
+    }
+
+    public void ShowAttackInfo(){
+        Console.WriteLine($"{Name} causes {DamageAmount}% damage");
+    }
+
+}
---
+++ C# .Net Part Time/Language Fundamentals/Core/Game Developer I/GameDev/Program.cs
@@ -0,0 +1,33 @@
+﻿//Create an instance of Enemy
+Enemy Guard = new Enemy("Guard");
+// Guard.ShowInfo();
+
+//Create instances of Attack:
+Attack Punch = new Attack("Punch", 10);
+Guard.AddAttack(Punch);
+Attack Kick = new Attack("Kick", 15);
+Guard.AddAttack(Kick);
+Attack FireBall = new Attack("Fire Ball", 25);
+Guard.AddAttack(FireBall);
+
+//Add these attacks to the enemy's AttackList:
+Guard.AddAttack(Punch);
+Guard.AddAttack(Kick);
+Guard.AddAttack(FireBall);
+
+//Display enemy information and perform random attacks:
+
+Guard.ShowInfo();
+
+Guard.RandomAttack();
+Guard.ShowHealth();
+Guard.RandomAttack();
+Guard.ShowHealth();
+Guard.RandomAttack();
+Guard.ShowHealth();
+Guard.RandomAttack();
+Guard.ShowHealth();
+Guard.RandomAttack();
+Guard.ShowHealth();
+Guard.RandomAttack();
+Guard.ShowHealth();
