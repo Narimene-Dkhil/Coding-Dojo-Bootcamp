adf42ddc86844519ee42b63ff6ab9d4797ec638a
narimenedk <narimene_dkhil@yahoo.com>
2024-08-04 11:50:32 +0100

    First connection

---
+++ C# .Net Part Time/ORMs/Practice/First Connection/FirstConnection/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstConnection.Models;
+
+namespace FirstConnection.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext _context;  //This is new
+
+    public HomeController(ILogger<HomeController> logger, MyContext context) // injection here
+    {
+        _logger = logger;
+        _context = context; // This is new
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
